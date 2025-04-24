f390c3bdfabd546da09dc50087fdf56e3f579845
Vitor <vitorpereira@profkenny.com.br>
2025-04-24 00:58:04 -0300

    Removendo rota temporaria para criar um usuario

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,3 @@ Route::get('/polls/{poll}', [PollController::class, 'show'])->name('polls.show')
 Route::post('/polls/vote', [PollVoteController::class, 'store'])->name('polls.vote');
 Route::delete('/polls/{poll}', [PollController::class, 'destroy'])->middleware('auth')->name('polls.destroy');
 
-Route::get('/create-admin', function () {
-    \App\Models\User::create([
-        'name' => 'Admin',
-        'email' => 'admin@example.com',
-        'password' => bcrypt('password@123'),
-    ]);
-    return 'Usuário criado com sucesso!';
-});
