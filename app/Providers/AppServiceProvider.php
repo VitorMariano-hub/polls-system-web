c5d98151effdc5b2b801c7164c09e356876fe657
Vitor <vitorpereira@profkenny.com.br>
2025-04-24 01:28:50 -0300

    Remove config boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,15 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
-            // Força todas as URLs geradas pelo Laravel a usarem HTTPS
-            URL::forceScheme('https');
-    
-            // Configura Laravel para confiar no proxy (como Railway)
-            Request::setTrustedProxies(
-                ['*'], // Aceita qualquer proxy, útil em plataformas como Railway ou Heroku
-                Request::HEADER_X_FORWARDED_ALL
-            );
-        }
+        //
     }
 }
