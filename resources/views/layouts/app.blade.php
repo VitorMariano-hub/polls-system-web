7564e820252c22c5d7f2b78c7be8a9e6479a372f
Vitor <vitorpereira@profkenny.com.br>
2025-04-24 01:44:50 -0300

    Adicionando metatag security

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Sistema de enquetes</title>
     <link rel="icon" href="/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
